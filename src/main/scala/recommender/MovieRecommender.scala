5b6bfaa0282527b33c72327e050cc8adf8b7ae9b
Tatjana Predojev <tatjana.predojev@springer.com>
2019-07-29 10:58:05 +0200

    WIP some improvements and TODOs

--- src/main/scala/recommender/MovieRecommender.scala
+++ src/main/scala/recommender/MovieRecommender.scala
@@ -2,8 +2,10 @@ package recommender
 
 import org.apache.spark.ml.evaluation.RegressionEvaluator
 import org.apache.spark.sql.types._
-import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 import org.apache.spark.ml.recommendation.ALS
+import org.apache.spark.mllib.evaluation.RankingMetrics
+import org.apache.spark.rdd.RDD
 
 object MovieRecommender extends App {
 
@@ -59,15 +61,45 @@ object MovieRecommender extends App {
   val model = als.fit(training)
 
   // Evaluate the model by computing the RMSE on the test data
+  // RMSE applicable because of explicit rating
   // Note we set cold start strategy to 'drop' to ensure we don't get NaN evaluation metrics
   model.setColdStartStrategy("drop")
-  val predictions = model.transform(test)
+  val predictions = model.setPredictionCol("prediction").transform(test)
 
   val evaluator = new RegressionEvaluator()
     .setMetricName("rmse")
     .setLabelCol("rating")
     .setPredictionCol("prediction")
+
   val rmse = evaluator.evaluate(predictions)
   println(s"Root-mean-square error = $rmse")
+
+  // wrong!
+  // https://stackoverflow.com/questions/37975715/rankingmetrics-in-spark-scala
+  // TODO: e.g. Compare with top ten most relevant documents
+  val testRatingCol: Array[Float] = test.select("rating").collect().map(_.getFloat(0))
+  val testPredictionCol: Array[Float] = predictions.select("prediction").collect().map(_.getFloat(0))
+  val metrics: RDD[(Array[Float], Array[Float])] = spark.createDataset(List((testPredictionCol, testRatingCol))).rdd
+  val rm = new RankingMetrics[Float](metrics)
+  println(s"Mean-average-precision = ${rm.meanAveragePrecision}")
+
+  // Generate top 10 movie recommendations for each user
+  val userRecs = model.recommendForAllUsers(10)
+  // Generate top 10 user recommendations for each movie
+  val movieRecs = model.recommendForAllItems(10)
+
+  // Generate top 10 movie recommendations for a specified set of users
+  val users = ratings.select(als.getUserCol).distinct().limit(3)
+  val userSubsetRecs = model.recommendForUserSubset(users, 10)
+  // Generate top 10 user recommendations for a specified set of movies
+  val movies = ratings.select(als.getItemCol).distinct().limit(3)
+  val movieSubSetRecs = model.recommendForItemSubset(movies, 10)
+
+  //userRecs.show()
+  //movieRecs.show()
+  //userSubsetRecs.show()
+  //movieSubSetRecs.show()
+
+  spark.stop()
 
 }
